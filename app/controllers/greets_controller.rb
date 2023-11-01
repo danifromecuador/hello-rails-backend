523026912992925ff4bd69c867aaec0613ce568b
dani <danifromecuador@gmail.com>
2023-11-01 12:30:48 -0500

    Create endpoint at greets/mesage

--- app/controllers/greets_controller.rb
+++ app/controllers/greets_controller.rb
@@ -1,5 +1,6 @@
 class GreetsController < ApplicationController
   def message
-    @message = Greet.all[rand(Greet.all.size)].message
+    rand_msg = Greet.all[rand(Greet.all.size)].message
+    render json: { message: rand_msg}
   end
 end
