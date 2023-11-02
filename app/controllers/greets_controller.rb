127403a8587b6bc5968316f123d876f608758067
dani <danifromecuador@gmail.com>
2023-11-01 19:28:08 -0500

    Install rubocop and stylelint linters

--- app/controllers/greets_controller.rb
+++ app/controllers/greets_controller.rb
@@ -1,6 +1,6 @@
 class GreetsController < ApplicationController
   def message
     rand_msg = Greet.all[rand(Greet.all.size)].message
-    render json: { message: rand_msg}
+    render json: { message: rand_msg }
   end
 end
